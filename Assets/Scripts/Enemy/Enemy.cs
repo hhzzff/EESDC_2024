03fadaf911b8a3b1599d36010bf4b1cca9cd3d60
sigureling <1678348940@qq.com>
2024-02-11 12:55:12 +0800

    Update enemy implementation

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour,IEnemy
+{
+    public EnemyInfo info;
+    private Renderer renderer=new Renderer();
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        Step2Center();
+        TakeDamage(1);
+    }
+    void Step2Center()
+    {
+        float norm=Mathf.Sqrt(transform.position.x*transform.position.x + transform.position.y*transform.position.y);
+        info.vel = new Vector2(-transform.position.x, -transform.position.y)/norm;
+        transform.position +=Time.deltaTime* new Vector3(info.vel.x, info.vel.y, 0);
+        info.pos = new Vector2(transform.position.x,transform.position.y);
+    }
+    public void TakeDamage(int damage)
+    {
+        FlashWhite();
+        info.hp -= damage; //hp½»¸ømanager¼ì²é
+        Debug.Log(info.hp);
+
+    }
+    private IEnumerator FlashWhite()
+    {
+        renderer.material.color = Color.white;
+        yield return new WaitForSeconds(1f);
+        renderer.material.color = Color.black;
+    }
+
+}
--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,35 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyManager : SingletonMono<EnemyManager>,IEnemyManager
 {
     // Start is called before the first frame update
-    List<EnemyBase> enemies = new List<EnemyBase>();
+    public Enemy myPrefab;
+    public Dictionary<EnemyType,int> hpDic = new Dictionary<EnemyType,int>();
+    List<Enemy> enemies = new List<Enemy>();
+    Vector3 rightUp;
+    Vector3 leftDown;
+    float right;
+    float left;
+    float up;
+    float down;
     void Start()
     {
-
+        rightUp = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        leftDown=Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        right = rightUp.x;
+        left = leftDown.x;
+        up = rightUp.y;
+        down =leftDown.y;
+        hpDic.Add(EnemyType.Basic, 100);
+        //Add.....
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckHp();
-        GenerateEnemies();
+        GenerateEnemy();
         
     }
-    public void CheckHp()  //销毁Hp清空的敌人
+    void GenerateEnemy() //增加新的敌人，考虑随Time.time数目递增，random初始化
     {
-
+        int generateNum = (int)(Time.time % 10)+1;  //之后考虑更换为更好的递增策略
+        float x,y;
+        for(int i=0;i<generateNum;i++)
+        {
+            x = Random.Range(left - 5, right + 5);
+            if (x < left || x > right)
+                y = Random.Range(down - 5, up + 5);
+            else
+            {
+                if (Random.value < 0.5f)
+                    y = Random.Range(down - 5, down);
+                else
+                    y = Random.Range(up, up + 5);
+            }
+            Enemy newEnemy = Instantiate(myPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            newEnemy.info.hp = hpDic[EnemyType.Basic];
+            newEnemy.info.type = EnemyType.Basic;
+            enemies.Add(newEnemy);
+        }
     }
-    public void GenerateEnemies() //增加新的敌人，考虑随Time.time数目递增，random初始化
+    void RemoveEnemy(Enemy enemy)
     {
-
+        if(enemies.Contains(enemy))
+        {
+            Debug.Log("Enemy Dies");
+            enemies.Remove(enemy);
+            Destroy(enemy.gameObject);
+        }
     }
+    void CheckHp()  //销毁Hp清空的敌人
+    {
+        for (int i = enemies.Count - 1; i >= 0; i--)
+        {
+            Enemy enemy = enemies[i];
+            if (enemy.info.hp <= 0)
+            {
+                RemoveEnemy(enemy);
+            }
+        }
+    }
+
     public List<EnemyInfo> GetEnemyList()  //返回info
     {
         List<EnemyInfo> enemyInfos = new List<EnemyInfo>();
-        foreach(EnemyBase enemy in enemies)
+        foreach(Enemy enemy in enemies)
             enemyInfos.Add(enemy.info);
         return enemyInfos;
     }//获取所有已生成的敌人信息
