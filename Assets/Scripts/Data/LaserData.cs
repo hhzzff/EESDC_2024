9d5e59935fe815f0b0d2c8b16a94c12b6cc968c5
hhzzff <2070620600@qq.com>
2024-02-16 18:51:52 +0800

    feat: :sparkles: Add more accurate ray tracing and bullet stage changes to the turret.

--- Assets/Scripts/Data/LaserData.cs
+++ Assets/Scripts/Data/LaserData.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 [CreateAssetMenu(fileName = "LaserData", menuName = "GameData/LaserData", order = 0)]
 public class LaserData : ScriptableObject
 {
-    public int damage;
-    public float speed;
+    public int[] damage;
+    public float[] speed;
+    public float lifeTime;
 }
